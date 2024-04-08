0bd2f00d2d2c98ba16ac3b42fda44b17cfb91c70
mantr002 <rotenda.mantsha@vcontractor.co.za>
2024-04-08 07:31:17 +0200

    Removed ununsed imports

--- code-spectacles-client/code-spectacles-client/Endpoints.cs
+++ code-spectacles-client/code-spectacles-client/Endpoints.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using static code_spectacles_client.Program;
+﻿using System.Net.Http.Headers;
 using code_spectacles_client.AllEndPoints;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Net.Http.Json;
 namespace code_spectacles_client
 {
 
